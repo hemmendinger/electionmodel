8a3a3345e9f57ffa32b41eb276f0c1d0f591aafd
Hemmendinger <github@hemmendinger.net>
2017-11-03 16:15:54 -0400

    several emodel methods written, consider methods as unproven unless tests exist at this point

--- emodel.py
+++ emodel.py
@@ -9,22 +9,66 @@ class Election:
 
     """
 
-    def __init__(self, election_day: str, polls: pd.DataFrame, choices: list):
-        self.election_day = datetime.datetime.strptime(election_day, '%Y-%m-%d')
-        self.days_remaining = (election_day - datetime.datetime.today()).days
+    def __init__(self, polls: pd.DataFrame):
+        self.election_day = None
+        self.days_remaining = None
 
         self.polls = polls
 
+        # Includes any response of uncertainty: "undecided"
+        # TODO: include "refused" and exclude from self.responses
+        self.responses_uncertain = None
         # Candidates, ballot measures, etc.
-        # Includes "other"
-        # Excludes "undecided" and "refused"
-        self.choices = choices
+        # Includes "other", etc.
+        self.responses = None
 
+        self.interest = None
+
+
+    def _fillnan_undecided(self):
         # Fill nan in undecided with: 100 - sum(choices)
         if 'undecided' in self.polls.columns:
-            self.polls['undecided'].fillna(
-                100 - polls[choices].fillna(0).sum(axis=1),
-                inplace=True)
+
+            self.polls['undecided'].fillna(100 - self.polls[self.response].fillna(0).sum(axis=1), inplace=True)
+            #self.polls['undecided]
+
+    def set_election_day(self, election_day):
+        if type(election_day) is datetime.datetime:
+            self.election_day = election_day
+        elif type(election_day) is str:
+            self.election_day = datetime.datetime.strptime(election_day, '%Y-%m-%d')
+
+        self.days_remaining = (self.election_day - datetime.datetime.today()).days
+
+        if self.days_remaining < 0:
+            self.days_remaining = 0
+
+    def set_interest(self, interest):
+        """
+        :param interest: List of column names that represent poll responses of interest.
+        :return:
+        """
+        self.interest = interest.copy()
+
+    def set_responses(self, responses):
+        """
+        :param responses: List of any column names that represent poll responses.
+        :return:
+        """
+        self.responses = list()
+        for resp in responses:
+            if resp == 'undecided':
+                self.responses_uncertain = ['undecided']
+            else:
+                self.responses.append(resp)
+
+
+
+
+
+
+
+
 
 
 
